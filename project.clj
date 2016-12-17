4c9a14f6c63e043b1d26a10dec451c67504aa455
Karthik Sudana <ksudana@Karthiks-MacBook-Pro.local>
2016-12-17 00:23:49 -0600

    updated project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject adventure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject adventure "Rescuing-Mattox-1.0"
+  :description "A text-based adventure game written in Clojure."
+  :url "https://github.com/ksudana/CS296Project"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
